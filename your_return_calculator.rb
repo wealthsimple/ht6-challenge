d3d796314d9499d3934f99b2150eeffad8e5db83
Imran Ariffin <ariffinimran@gmail.com>
2017-08-26 14:50:39 -0400

    imran try 1

--- your_return_calculator.rb
+++ your_return_calculator.rb
@@ -10,6 +10,26 @@ class YourReturnCalculator < ReturnCalculator
     # snapshot.cash_flow
     # snapshot.market_value
 
-    BigDecimal.new(1)
+    ret = snapshots.map.with_index{|s, i|
+        s[:i] = i
+        s
+    }
+    .reduce(Array.new){|ret, s|
+        if s[:i] == 0
+            ret
+        else
+            cur = snapshots.at(s[:i])
+            prev = snapshots.at(s[:i] - 1)
+            e = (cur[:market_value] - cur[:cash_flow]) 
+            e /= prev[:market_value]
+            e -= 1
+            ret.push(e)
+        end
+    }
+    .reduce(1) {|ret, naive|
+        ret * (1 + naive)
+    } - 1
+
+    BigDecimal.new(ret)
   end
 end
